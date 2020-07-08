ea97923714441d844083d34204d588ed8a5e07d7
KhaledBousrih <khaled.bousrih@netsach.fr>
2020-07-08 11:03:30 +0200

    use object_name instead of name to rearrange the admin models in categories

--- concrete_datastore/admin/admin_site.py
+++ concrete_datastore/admin/admin_site.py
@@ -10,9 +10,14 @@ from concrete_datastore.admin.admin_form import (
     OTPAuthenticationForm,
 )
 
-
-EXTRA_MODEL_NAMES_LOWER = ('emails',)
-AUTH_MODEL_NAMES_LOWER = ('users', 'groups', 'tokens', 'permissions', 'roles')
+EXTRA_MODEL_OBJECT_NAMES = ('Email',)
+AUTH_MODEL_OBJECT_NAMES = (
+    'User',
+    'Group',
+    'AuthToken',
+    'ConcretePermission',
+    'ConcreteRole',
+)
 
 
 def get_admin_site():
@@ -111,8 +116,8 @@ def get_admin_site():
             'models': [
                 model
                 for model in models
-                if model['name'].lower()
-                not in EXTRA_MODEL_NAMES_LOWER + AUTH_MODEL_NAMES_LOWER
+                if model['object_name']
+                not in EXTRA_MODEL_OBJECT_NAMES + AUTH_MODEL_OBJECT_NAMES
             ],
         }
         extra_group_models = {
@@ -121,7 +126,7 @@ def get_admin_site():
             'models': [
                 model
                 for model in models
-                if model['name'].lower() in EXTRA_MODEL_NAMES_LOWER
+                if model['object_name'] in EXTRA_MODEL_OBJECT_NAMES
             ],
         }
         auth_group_models = {
@@ -130,7 +135,7 @@ def get_admin_site():
             'models': [
                 model
                 for model in models
-                if model['name'].lower() in AUTH_MODEL_NAMES_LOWER
+                if model['object_name'] in AUTH_MODEL_OBJECT_NAMES
             ],
         }
         custom_app_groups_list = [
