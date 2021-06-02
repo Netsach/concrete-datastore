340f27f1b7e93f6db279778162c3044a10b4572a
Souleymane TOURÉ <souleymane.toure@netsach.fr>
2021-06-02 11:01:52 +0200

    Use function to check if value is uuid

--- concrete_datastore/api/v1/filters.py
+++ concrete_datastore/api/v1/filters.py
@@ -29,6 +29,15 @@ RANGEABLE_TYPES = (
 )
 
 
+def ensure_uuid_valid(value):
+    try:
+        uuid.UUID(value)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 def convert_type(string, field_type, close_period=True):
     if string == '':
         raise ValidationError(
@@ -216,9 +225,7 @@ class FilterSupportingOrBackend(
                 (UUIDField, ForeignKey),
             ):
                 for value in values:
-                    try:
-                        uuid.UUID(value)
-                    except ValueError:
+                    if not ensure_uuid_valid(value):
                         raise ValidationError(
                             {
                                 "message": (
