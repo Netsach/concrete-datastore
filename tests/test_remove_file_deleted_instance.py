bb0ce5558c76caf45812568cc5afef6fd5f34dee
KhaledBousrih <khaled.bousrih@netsach.fr>
2021-06-08 16:34:57 +0200

    Add checks when deleting files

--- tests/test_remove_file_deleted_instance.py
+++ tests/test_remove_file_deleted_instance.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import io
 import os
+from mock import patch
 from django.test import TestCase
 from concrete_datastore.concrete.models import Project, DeletedModel
 from django.test import override_settings
@@ -22,8 +23,34 @@ class FileFieldDeleteTestCase(TestCase):
 
     def test_file_removed_non_existing_file(self):
         project = Project.objects.create(name='project test')
-        with self.assertRaises(Exception):
-            project.picture.path
         self.assertEqual(DeletedModel.objects.count(), 0)
         project.delete()
         self.assertEqual(DeletedModel.objects.count(), 1)
+
+    def test_file_removed_file_path_doesnot_exist(self):
+        project_picture = io.BytesIO()
+        project_picture.write(b'fake image data')
+        project = Project.objects.create(name='project test')
+        project.picture.save('picture.jpg', project_picture)
+        project_picture_path = project.picture.path
+        os.remove(project_picture_path)
+        self.assertEqual(DeletedModel.objects.count(), 0)
+        project.delete()
+        self.assertEqual(DeletedModel.objects.count(), 1)
+
+    def test_file_removed_file_delete_error(self):
+        def fake_remove(*args, **kwargs):
+            raise ValueError
+
+        patch(
+            'concrete_datastore.concrete.automation.signal_processor.os.remove',
+            new=fake_remove,
+        ).start()
+        project_picture = io.BytesIO()
+        project_picture.write(b'fake image data')
+        project = Project.objects.create(name='project test')
+        project.picture.save('picture.jpg', project_picture)
+        self.assertEqual(DeletedModel.objects.count(), 0)
+        project.delete()
+        self.assertEqual(DeletedModel.objects.count(), 1)
+        patch.stopall()
