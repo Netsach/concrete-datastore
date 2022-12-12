dfb611e39c8168be508e9c73cc9d7e3587a05db6
KhaledBousrih <khaled.bousrih@netsach.fr>
2022-12-12 11:31:03 +0100

    Fix mfa validation to set an totp device

--- concrete_datastore/routes/forms.py
+++ concrete_datastore/routes/forms.py
@@ -1,29 +1,18 @@
 # conding: utf-8
+from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
-from django import forms
-from concrete_datastore.concrete.constants import MFA_OTP
 from django.utils.translation import ngettext_lazy, gettext_lazy as _
-from concrete_datastore.concrete.constants import MFA_EMAIL
+from concrete_datastore.concrete.constants import MFA_OTP, MFA_EMAIL
 
 
 class ConfigureOTPLoginForm(forms.Form):
-    """
-    TODO:
-    Create a code with an emaildevice with MFA_EMAIL.
-    Retrieve the last emaildevice with MFA_EMAIL (do not check the field `confirmed`)
-    or create it if it does not exist and generate a challenge.
-    Then verify the code with the same device when the user is authenticated
-    """
 
     otp_error_messages = {
         'token_required': _('Please enter your OTP token.'),
         'challenge_exception': _('Error generating challenge: {0}'),
         'challenge_message_email': _(
             'Enter the two authentication code you received by email'
-        ),
-        'challenge_message_otp': _(
-            'Enter the two-factor authentication code from a configured OTP application'
         ),
         'invalid_token': _(
             'Invalid token. Please make sure you have entered it correctly.'
@@ -36,6 +25,8 @@ class ConfigureOTPLoginForm(forms.Form):
         'verification_not_allowed': _(
             "Verification of the token is currently disabled"
         ),
+        'otp_already_configured': _('OTP authentication already configured'),
+        'wrong_auth_credentials': _('Wrong auth credentials'),
     }
     email = forms.EmailField(label="Email", max_length=254)
     password = forms.CharField(label="Password", max_length=254)
@@ -56,43 +47,61 @@ class ConfigureOTPLoginForm(forms.Form):
         User = get_user_model()
         self.user_cache = User.objects.filter(email=email).first()
         if self.user_cache is None:
-            raise ValidationError('Wrong auth credentials')
+            raise ValidationError(
+                self.otp_error_messages['wrong_auth_credentials'],
+                code='wrong_auth_credentials',
+            )
         if self.user_cache.check_password(password) is False:
-            raise ValidationError('Wrong auth credentials')
+            #: Clear user cache to reset the form
+            self.user_cache = None
+            raise ValidationError(
+                self.otp_error_messages['wrong_auth_credentials'],
+                code='wrong_auth_credentials',
+            )
         otp_device = self.user_cache.emaildevice_set.filter(
             mfa_mode=MFA_OTP, confirmed=True
         ).first()
         if otp_device:
             #: Clear user cache to reset the form
             self.user_cache = None
-            raise ValidationError('OTP authentication already configured')
+            raise ValidationError(
+                self.otp_error_messages['otp_already_configured'],
+                code='otp_already_configured',
+            )
+        device = self._chosen_device(self.user_cache)
         if not otp_token:
-            device = self._chosen_device(self.user_cache)
             self._handle_challenge(device)
+        else:
+            verified = device._verify_email_token(otp_token)
+            if verified is False:
+                #: Keep user cache to avoid selecting an email and password
+                raise ValidationError(
+                    self.otp_error_messages['invalid_token'],
+                    code='invalid_token',
+                )
         return cleaned_data
 
     def _handle_challenge(self, device):
-        challenge_message_name = 'challenge_message_otp'
-        if device.mfa_mode == MFA_EMAIL:
-            challenge_message_name = 'challenge_message_email'
-            try:
-                device.generate_challenge()
-            except Exception as e:
-                raise forms.ValidationError(
-                    self.otp_error_messages['challenge_exception'].format(e),
-                    code='challenge_exception',
-                )
+        try:
+            device.generate_challenge()
+        except Exception as e:
+            raise forms.ValidationError(
+                self.otp_error_messages['challenge_exception'].format(e),
+                code='challenge_exception',
+            )
 
         #:  If the challenge is successfully generated and in order to show
         #:  the appropriate message, a `ValidationError` must be raised.
         #:  *NB* This validation error does not mean that an error occured
         raise forms.ValidationError(
-            self.otp_error_messages[challenge_message_name],
-            code=challenge_message_name,
+            self.otp_error_messages['challenge_message_email'],
+            code='challenge_message_email',
         )
 
     def _chosen_device(self, user):
-        device = user.emaildevice_set.filter(confirmed=True).last()
+        device = user.emaildevice_set.filter(
+            confirmed=True, mfa_mode=MFA_EMAIL
+        ).first()
         if not device:
             device = user.emaildevice_set.create(
                 email=user.email, name='User default email', confirmed=True
